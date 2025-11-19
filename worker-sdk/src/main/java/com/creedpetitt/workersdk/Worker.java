3f7d96291d7034e2701d06fe29c345cce227a934
Creed Petitt <creedpetitt@gmail.com>
2025-11-19 00:10:49 -0600

    create Worker template

--- worker-sdk/src/main/java/com/creedpetitt/workersdk/Worker.java
+++ worker-sdk/src/main/java/com/creedpetitt/workersdk/Worker.java
@@ -1,6 +1,24 @@
 package com.creedpetitt.workersdk;
 
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Worker {
+
+    private final Map<String, Handler> handlers = new HashMap<>();
+    private KafkaConsumer<String, JobMessage> consumer;
+    private KafkaProducer<String, ResultMessage> producer;
+
+    public void register(String action, Handler handler) {
+
+    }
+
+    public void start() {
+
+    }
 
 }
 
