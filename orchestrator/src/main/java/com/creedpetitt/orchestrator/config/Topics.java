3ad6517068a88c78421c9e680afa53712cd61400
Creed Petitt <creedpetitt@gmail.com>
2025-11-19 23:14:54 -0600

    add Topics class for Kakfa topic names

---
+++ orchestrator/src/main/java/com/creedpetitt/orchestrator/config/Topics.java
@@ -0,0 +1,6 @@
+package com.creedpetitt.orchestrator.config;
+
+public class Topics {
+    public static final String WORKFLOW_JOBS = "workflow-jobs";
+    public static final String WORKFLOW_RESULTS = "workflow-results";
+}
