8f5831aeaa48af3cf55ef0ef6a28b6d145f480ae
Creed Petitt <creedpetitt@gmail.com>
2025-11-22 23:58:08 -0600

    init StepResult JPA entity as child to WorkflowRun

---
+++ orchestrator/src/main/java/com/creedpetitt/orchestrator/model/StepResult.java
@@ -0,0 +1,34 @@
+package com.creedpetitt.orchestrator.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "step_result")
+public class StepResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "workflow_run_id")
+    @JsonIgnore
+    private WorkflowRun workflowRun;
+
+    @Column
+    private String action;
+
+    @Column
+    private int stepIndex;
+
+    @Column(columnDefinition = "TEXT")
+    private String result;
+
+    @Column
+    private LocalDateTime timestamp;
+}
