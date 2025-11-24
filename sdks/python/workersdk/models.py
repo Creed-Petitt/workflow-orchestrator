1072db13c913a268a4edec123186fb761cac8664
Creed Petitt <creedpetitt@gmail.com>
2025-11-23 19:51:39 -0600

    add JobMessage and ResultMessage data classes

---
+++ sdks/python/workersdk/models.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class JobMessage:
+    workflow_run_id: str
+    action: str
+    payload: str
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            workflow_run_id=data['workflowRunId'],
+            action=data['action'],
+            payload=data['payload']
+        )
+
+
+@dataclass
+class ResultMessage:
+    workflow_run_id: str
+    action: str
+    result: str
+
+    def to_dict(self):
+        return {
+            'workflowRunId': self.workflow_run_id,
+            'action': self.action,
+            'result': self.result
+        }
