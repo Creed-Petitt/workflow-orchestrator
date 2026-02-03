5aef58c1265e2dc06dafed08ae8edccc70da7418
Creed Petitt <creedpetitt@gmail.com>
2026-02-03 17:54:52 -0600

    add endpoints for retrieving all workflows and all runs

--- orchestrator/src/main/java/com/creedpetitt/orchestrator/controller/WorkflowController.java
+++ orchestrator/src/main/java/com/creedpetitt/orchestrator/controller/WorkflowController.java
@@ -2,10 +2,13 @@ package com.creedpetitt.orchestrator.controller;
 
 import com.creedpetitt.orchestrator.dto.CreateWorkflowRequest;
 import com.creedpetitt.orchestrator.dto.TriggerWorkflowRequest;
+import com.creedpetitt.orchestrator.model.WorkflowDefinition;
 import com.creedpetitt.orchestrator.model.WorkflowRun;
 import com.creedpetitt.orchestrator.service.WorkflowService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/workflow")
@@ -23,9 +26,18 @@ public class WorkflowController {
         return ResponseEntity.ok(id);
     }
 
+    @GetMapping("/")
+    public ResponseEntity<List<WorkflowDefinition>> getAllWorkflows() {
+        return ResponseEntity.ok(workflowService.getAllWorkflows());
+    }
+
     @PostMapping("/{id}/trigger")
-    public ResponseEntity<String> triggerWorkflow(@PathVariable String id, @RequestBody TriggerWorkflowRequest req) {
-        String runId = workflowService.triggerWorkflow(id, req);
+    public ResponseEntity<String> triggerWorkflow(
+            @PathVariable String id,
+            @RequestBody TriggerWorkflowRequest req,
+            @RequestHeader(value = "Idempotency-Key", required = false) String idempotencyKey
+    ) {
+        String runId = workflowService.triggerWorkflow(id, req, idempotencyKey);
         return ResponseEntity.ok(runId);
     }
 
@@ -33,5 +45,10 @@ public class WorkflowController {
     public ResponseEntity<WorkflowRun> getWorkflow(@PathVariable String runId) {
         WorkflowRun run = workflowService.getRunStatus(runId);
         return ResponseEntity.ok(run);
+    }
+
+    @GetMapping("/runs")
+    public ResponseEntity<List<WorkflowRun>> getAllRuns() {
+        return ResponseEntity.ok(workflowService.getAllRuns());
     }
 }
